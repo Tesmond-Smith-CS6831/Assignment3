c2c4b6c05c69a74a1912b9abe660e1a506f51265
Jordan Smith <jsmit018@odu.edu>
2021-03-13 16:19:10 -0500

    Removed ZMQ files from subxcirber and publisher and filtered them into the middleware/broker layer

--- middleware.py
+++ middleware.py
@@ -31,11 +31,36 @@ class Broker:
             self.backend_socket.bind(f"tcp://*:{self.back}")
             zmq.proxy(self.frontend_socket, self.backend_socket)
 
+    def register_pub(self, publisher):
+        publisher.context = zmq.Context()
+        publisher.socket = publisher.context.socket(zmq.PUB)
+        publisher.socket.connect(f"tcp://{self.host}:{self.port}")
+        return publisher
 
-if __name__ == "__main__":
-    # ip_address = sys.argv[1] if len(sys.argv) > 1 else "localhost"
-    # print("Sysarg 1. Publisher connection port, 2. Subscriber connection port")
-    socket_to_pub = sys.argv[1] if len(sys.argv) > 1 else "6663"
-    socket_to_sub = sys.argv[2] if len(sys.argv) > 2 else "5556"
-    broker = Broker(socket_to_pub, socket_to_sub)
-    broker.establish_broker()
+    def pub_send(self, publisher, message, proxy = 2):
+        zipcode, temperature, date_time = message.split(',')
+        if proxy == 1:
+            publisher.socket.send_string("{},{},{}".format(zipcode, temperature, date_time))
+        else:
+            publisher.bind(f"tcp://*:{publisher.port + 1}")
+            publisher.socket.send_string("{},{},{}".format(zipcode, temperature, date_time))
+
+    def register_sub(self, subscriber):
+        subscriber.context = zmq.Context()
+        subscriber.socket = subscriber.context.socket(zmq.SUB)
+        subscriber.socket.connect(f"tcp://{subscriber.address}:{subscriber.port}")
+        subscriber.socket.setsockopt_string(zmq.SUBSCRIBE, subscriber.zip_code)
+        return subscriber
+
+    def filter_message(self, subscriber):
+        subscriber.message = subscriber.socket.recv_string()
+        return subscriber
+
+
+# if __name__ == "__main__":
+# ip_address = sys.argv[1] if len(sys.argv) > 1 else "localhost"
+# print("Sysarg 1. Publisher connection port, 2. Subscriber connection port")
+socket_to_pub = sys.argv[1] if len(sys.argv) > 1 else "6663"
+socket_to_sub = sys.argv[2] if len(sys.argv) > 2 else "5556"
+universal_broker = Broker(socket_to_pub, socket_to_sub)
+universal_broker.establish_broker()
