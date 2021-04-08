ff4ae11e47539db11c7f6d820b50ff861fb0d77c
Rick Tesmond <ricktesmond@Ricks-MBP.attlocal.net>
2021-04-07 23:46:31 -0500

    created nodes that track ip addr, next step is loadbalance app

--- middleware.py
+++ middleware.py
@@ -10,6 +10,7 @@ Initial thoughts from Rick T:
     - this allows for neither the client/server knowing the pub/sub servers
 """
 import sys
+import socket
 import zmq
 import random
 from kazoo.client import KazooClient
@@ -30,6 +31,7 @@ class Broker:
         self.connection_count = 0
         self.zookeeper = KazooClient(hosts='127.0.0.1:2181')
         self.zookeeper.start()
+        self.ip_addr = socket.gethostbyname(socket.gethostname())
 
     def gen_nodes(self):
         leader_path = "{}{}".format(self.zk_leader_path, "leadNode")
@@ -49,58 +51,59 @@ class Broker:
                 used_ports.append(port2)
 
                 node_name = "{}{}".format(self.zk_path, "node"+str(i))
-                ports = bytes("{},{}".format(port1, port2).encode("utf-8"))
+                node_info = bytes("{},{},{}".format(port1, port2, self.ip_addr).encode("utf-8"))
                 if self.zookeeper.exists(node_name):
                     pass
                 else:
                     self.zookeeper.ensure_path(self.zk_path)
-                    self.zookeeper.create(node_name,ports)
+                    self.zookeeper.create(node_name, node_info)
             self.set_leader()
 
     def set_leader(self):
+        import pdb;
+        pdb.set_trace()
         gen_leader = self.zookeeper.Election(self.zk_path, "leader").contenders()
         if gen_leader:
             self.leader = gen_leader[-1]
         else:
-            _nv = random.randint(0,4)
+            _nv = random.randint(0, 4)
             self.leader = self.zookeeper.get("{}node{}".format(self.zk_path, _nv))
+            [self.zookeeper.delete("{}{}".format(self.zk_path, "node" + str(i))) for i in range(5)]
         print("New leader node: {}".format(self.leader))
         leader_path = "{}{}".format(self.zk_leader_path, "leadNode")
-
-        if self.zookeeper.exists(leader_path):
-            self.zookeeper.delete(leader_path)
         self.zookeeper.ensure_path(leader_path)
-        self.zookeeper.set(leader_path, self.leader[0])
+        self.zookeeper.create(leader_path, self.leader[0])
 
     def create_loadbalance_replicas(self):
         """Create ephemeral (session bound) replicas of node"""
-        if not self.zookeeper.exists(self.replica_path):
-            print("Replicating leader...")
-            for i in range(0, 10):
-                replica_node = "{}{}".format(self.replica_path, "replicaNode{}".format(i))
-                self.zookeeper.ensure_path(self.replica_path)
-                self.zookeeper.create(replica_node, self.leader[0], ephemeral=True)
-            print("... Replication Complete")
+        print("Replicating leader...")
+        repl_id = self.zookeeper.get(self.replica_path)[1].numChildren
+        replica_node = "{}{}".format(self.replica_path, "replicaNode{}".format(repl_id))
+        l_ports = self.zookeeper.get("/leader/leadNode")[0].decode("utf-8")
+        repl_data = bytes("{},{}".format(l_ports, self.ip_addr).encode("utf-8"))
+        self.zookeeper.ensure_path(self.replica_path)
+        self.zookeeper.create(replica_node, repl_data, ephemeral=True)
+        print("... Replication Complete")
 
     def establish_broker(self):
-        @self.zookeeper.DataWatch(self.zk_leader_path)
-        def watch_node(data, stat, event):
-            if event:
-                leader_path = "{}{}".format(self.zk_leader_path, "leadNode")
-                print("Load balancer event detected: {}".format(event.type))
-                _nv = random.randint(0, 9)
-                self.leader = self.zookeeper.get("{}replicaNode{}".format(self.replica_path, _nv))
-                self.zookeeper.ensure_path(leader_path)
-                self.zookeeper.set(leader_path, self.leader[0])
-                self.zookeeper.delete(self.replica_path)
-                self.create_loadbalance_replicas()
         print("Loadbalanced Broker established. RUNNING.")
         leader_connection_addr = self.leader[0].decode('utf-8').split(',')
         self.context = zmq.Context()
         self.frontend_socket = self.context.socket(zmq.XSUB)
         self.backend_socket = self.context.socket(zmq.XPUB)
-        self.frontend_socket.bind(f"tcp://*:{leader_connection_addr[0]}")
-        self.backend_socket.bind(f"tcp://*:{leader_connection_addr[1]}")
+        self.frontend_socket.connect(f"tcp://{self.ip_addr}:{leader_connection_addr[0]}")
+        self.backend_socket.connect(f"tcp://{self.ip_addr}:{leader_connection_addr[1]}")
+        # @self.zookeeper.DataWatch(self.zk_leader_path)
+        # def watch_node(data, stat, event):
+        #     if event:
+        #         leader_path = "{}{}".format(self.zk_leader_path, "leadNode")
+        #         print("Load balancer event detected: {}".format(event.type))
+        #         _nv = random.randint(0, 9)
+        #         self.leader = self.zookeeper.get("{}replicaNode{}".format(self.replica_path, _nv))
+        #         self.zookeeper.ensure_path(leader_path)
+        #         self.zookeeper.set(leader_path, self.leader[0])
+        #         self.zookeeper.delete(self.replica_path)
+        #         self.create_loadbalance_replicas()
         zmq.proxy(self.frontend_socket, self.backend_socket)
 
 
@@ -111,7 +114,7 @@ def publish_node_conn(publish_obj):
 
     if up_status:
         data, stat = publish_obj.zookeeper.get(publish_obj.zk_path)
-        publish_obj.port = data.decode('utf-8').split(',')[0]
+        publish_obj.port, publish_obj.host = data.decode('utf-8').split(',')[0], data.decode('utf-8').split(',')[2]
         conn_str = "tcp://" + publish_obj.host + ":" + publish_obj.port
         publish_obj.socket.connect(conn_str)
         print("ZK node connected")
@@ -126,7 +129,7 @@ def subscribe_node_conn(subscriber_obj):
 
     if up_status:
         data, stat = subscriber_obj.zookeeper.get(subscriber_obj.zk_path)
-        subscriber_obj.port = data.decode('utf-8').split(',')[1]
+        subscriber_obj.port, subscriber_obj.address = data.decode('utf-8').split(',')[1], data.decode('utf-8').split(',')[2]
         print("ZK node connected")
     else:
         print("ZK not available")
@@ -136,4 +139,8 @@ if __name__ == "__main__":
     broker = Broker()
     broker.gen_nodes()
     broker.create_loadbalance_replicas()
-    broker.establish_broker()
+    try:
+        broker.establish_broker()
+    except KeyboardInterrupt:
+        broker.zookeeper.delete('/leader/leadNode')
+        print("Broker Node deleted")
